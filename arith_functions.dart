79fb60ec188b103e3a61eb4be1ceb224c340bfff
Ryan <biggiesmallzz0101@gmail.com>
2024-03-12 12:39:58 +0300

    Combined Dart functions

---
+++ arith_functions.dart
@@ -0,0 +1,80 @@
+/*
+import 'dart:io';
+void main () {
+  int addTwo(){
+    print("Enter first number:");
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int add = num1 + num2;
+    return add;
+  }
+  print("sum is  : ${addTwo()}");
+}
+*/
+
+/*
+import 'dart:io';
+void main () {
+  int subtractTwo(){
+    print("Enter first number:");
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int sub = num1 - num2;
+    return sub;
+  }
+  print("subtraction is  : ${subtractTwo()}");
+}
+*/
+
+/*
+import 'dart:io';
+void main () {
+  int multiplyTwo(){
+    print("Enter first number:");
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int mul = num1 * num2;
+    return mul;
+  }
+  print("multiplication is  : ${multiplyTwo()}");
+}
+*/
+
+/*
+import 'dart:io';
+void main () {
+  num divideTwo(){
+    print("Enter first number:");
+    num num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    num num2 = int.parse(stdin.readLineSync()!);
+    num div = num1 / num2;
+    return div;
+  }
+  print("division is  : ${divideTwo()}");
+}
+*/
+
+/*
+int stringLength(String str) {
+  return str.length;
+}
+
+void main() {
+  String myString = "Hello, world!";
+  int length = stringLength(myString);
+  print("The length of the string is: $length");
+}
+*/
+
+/*
+dynamic getFirstElement(List list) => list.isEmpty ? null : list.first;
+
+void main() {
+  List<String> fruits = ["Apple", "Banana", "Orange"];
+  print("The first element of the fruits list is: ${getFirstElement(fruits)}");
+}
+*/
